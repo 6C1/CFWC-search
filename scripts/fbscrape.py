106f4e6708f5dbbb27abdbac4700f65bd831f012
TLDuhamel <timothee.duhamel@gmail.com>
2014-09-28 15:31:43 +0200

    Removed unnecessary heavy files. FBScrape.py now produces posts.json directly

--- scripts/fbscrape.py
+++ scripts/fbscrape.py
@@ -3,37 +3,49 @@ import urllib2
 import json
 import re
 
+# removes unwanted bloat from JSON returned from Graph API
+def addJSONtoCollection(collection, objects):
+    for obj in objects :
+        if 'comments' in obj :
+            obj['comments'].pop('paging', None)
+            obj['comments'].pop('data', None)
+            obj['comments'].pop('summary.order', None)
+        if 'likes' in obj :
+            obj['likes'].pop('paging', None)
+            obj['likes'].pop('data', None)
+        obj.pop('updated_time', None)
+        collection.append(obj)
+
 def main():
     access_token = sys.argv[1]
     group_id = sys.argv[2]
-    url ="https://graph.facebook.com/%s/feed?access_token=%s"
-
-    url ="https://graph.facebook.com/%s?fields=feed.limit(10).fields(likes.summary(true),id,type,message,from,created_time)&access_token=%s"
-    url ="https://graph.facebook.com/%s?fields=feed.fields(created_time,comments.fields(created_time,from,id,like_count,parent,message),likes.fields(username,id),message,id,name,type,link)?access_token=%s"
-    url = "https://graph.facebook.com/%s?fields=feed.fields(created_time,comments.fields(created_time,from,id,like_count,parent,message).limit(200),likes.fields(username,id).limit(200),from,message,id,name,type,link)&method=GET&format=json&suppress_http_code=1&access_token=%s"
+    url = "https://graph.facebook.com/%s?fields=feed{from,created_time,message,likes.summary(true).limit(1),comments.summary(true).limit(1),link,name}&method=GET&format=json&suppress_http_code=1&access_token=%s"
     url = url % (group_id, access_token)
     
-    print url
-    requestsaver = open('saved_posts.txt', 'w')
-
+    print("Querying Graph API with following request:\n%s" % url)
     response = urllib2.urlopen(url)
     html = response.read()
-    requestsaver.write(html)
-    requestsaver.write("JSONSTREAMBREAK")
     parsed = json.loads(html)["feed"]
+    allposts = []
+    addJSONtoCollection(allposts, parsed["data"])
+    print("# of posts retrieved: %s" % len(allposts))
     
     #assumes there will be a next because i'm being lazy
     parsed['paging']['next'] = re.sub('limit=[0-9]*', 'limit=500', parsed['paging']['next'])
     while 'paging' in parsed:
-	if 'next' in parsed['paging']:
-	    response = urllib2.urlopen(parsed["paging"]["next"])
-	    html = response.read()
-	    print html
-	    requestsaver.write(html)
-	    requestsaver.write("JSONSTREAMBREAK")
-	    parsed = json.loads(html)
-	else:
-	    break
-    requestsaver.close()
+        if 'next' in parsed['paging']:
+            response = urllib2.urlopen(parsed["paging"]["next"])
+            html = response.read()
+            parsed = json.loads(html)
+            addJSONtoCollection(allposts, parsed["data"])
+            print("# of posts retrieved: %s" % len(allposts))
+        else:
+            break
+    print("Saving posts to posts.json...")
+    output = open('posts.json', 'w')
+    output.write("posts =")
+    output.write(json.dumps(allposts))
+    output.close()
+    print("Done!")
 if __name__ == "__main__":
     main()
